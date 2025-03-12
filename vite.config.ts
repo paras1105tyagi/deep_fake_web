3f734789896fcc563261f360b145138c91f026b7
Paras Tyagi <129530841+paras1105tyagi@users.noreply.github.com>
2025-03-12 19:53:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
-import { defineConfig } from 'vite'
- import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: process.env.NODE_ENV === "development"
-      ? {
-          "/api": {
-            target: "https://deepfake-api-c598.onrender.com",
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ""),
-          },
-        }
-      : undefined,
-  },
 });
